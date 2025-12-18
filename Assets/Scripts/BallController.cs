de6abecec6e6067193c878b83407e6f0f49c1933
Gabriel Vasile <g.vaSile@gmail.com>
2025-12-18 12:11:55 -0800

    Set up Physics so that the ball bounces off colliders. Made it so the ball doesn't rotate (Freeze Z) and so the paddles don't move when the ball hits them: Freeze X and Z

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+namespace Pong
+{
+    public class BallController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D myRigidBody;
+        [SerializeField] private float speed;
+
+        private void Awake()
+        {
+            AutoSetRigidBody();
+        }
+
+        private void AutoSetRigidBody()
+        {
+            if (myRigidBody != null)
+            {
+                return;
+            }
+
+            var rigidBoy = GetComponent<Rigidbody2D>();
+            if (rigidBoy != null)
+            {
+                myRigidBody = rigidBoy;
+            }
+        }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Start()
+        {
+            StartMovement();
+        }
+
+        private void StartMovement()
+        {
+            float x = 0;
+            float y = 0;
+
+            var randomNumber = Random.Range(0, 4);
+
+            switch (randomNumber)
+            {
+                case 0:
+                {
+                    x = 1;
+                    y = 1;
+                    break;
+                }
+                case 1:
+                {
+                    x = 1;
+                    y = -1;
+                    break;
+                }
+                case 2:
+                {
+                    x = -1;
+                    y = 1;
+                    break;
+                }
+                case 3:
+                {
+                    x = -1;
+                    y = -1;
+                    break;
+                }
+            }
+
+            myRigidBody.linearVelocity = new Vector2(x, y) * speed;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+    }
+}
