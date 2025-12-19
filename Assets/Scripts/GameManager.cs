427d2ffb740e33eb7762a3ee082ee4768809d65a
Gabriel Vasile <g.vaSile@gmail.com>
2025-12-18 16:55:19 -0800

    Added a canvas to show player socres. Updated colors for the game.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,33 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 namespace Pong
 {
     public class GameManager : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI player1ScoreText;
+        [SerializeField] private TextMeshProUGUI player2ScoreText;
+
         private int _player1Score = 0;
         private int _player2Score = 0;
+
+        private void Start()
+        {
+            player1ScoreText.text = _player1Score.ToString();
+            player2ScoreText.text = _player2Score.ToString();
+        }
 
         public void Player1Scored()
         {
             _player1Score++;
+            player1ScoreText.text = _player1Score.ToString();
         }
 
         public void Player2Scored()
         {
             _player2Score++;
+            player2ScoreText.text = _player2Score.ToString();
         }
     }
 }
